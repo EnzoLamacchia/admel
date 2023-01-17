1567f78f431cf0c14e2ac7006f65fe96cf97d8ae
Enzo Lamacchia <e.lamacchia@gmail.com>
2023-01-17 12:41:31 +0100

    sistemazione UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,6 +22,7 @@ class UserSeeder extends Seeder
                 'email' => 'superadmin@example.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'profile_id' => 1,
             ])->assignRole('amministratore');
     }
 }
